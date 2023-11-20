9f2cd98ba50bdde9c552a0bac8995a33f984bc82
khushmohammad <khush311407@gmail.com>
2023-11-20 17:32:33 +0530

    remove dist file

--- backend/dist/router/crudRoute.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.crud = void 0;
-const express_1 = require("express");
-const userController_1 = require("../controller/userController");
-const router = (0, express_1.Router)();
-router.get("/", userController_1.routTest);
-exports.crud = router;
--- backend/dist/router/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.user = exports.crud = void 0;
-const crudRoute_1 = require("./crudRoute");
-Object.defineProperty(exports, "crud", { enumerable: true, get: function () { return crudRoute_1.crud; } });
-const userRoute_1 = require("./userRoute");
-Object.defineProperty(exports, "user", { enumerable: true, get: function () { return userRoute_1.user; } });
